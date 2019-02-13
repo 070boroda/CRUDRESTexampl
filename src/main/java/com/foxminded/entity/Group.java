c6f32b3e06849f64255d1c5fac721a22e0744175
1 <1@boroda>
2019-02-13 12:11:49 +0300

    add entity

---
+++ src/main/java/com/foxminded/entity/Group.java
@@ -0,0 +1,35 @@
+package com.foxminded.entity;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@SuppressWarnings("serial")
+@NoArgsConstructor
+@Entity
+@Table(name = "groups")
+public class Group implements Serializable {
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	@Column(name="group_name")
+	private String groupName;
+	
+	@OneToMany(mappedBy="group", fetch=FetchType.LAZY)
+	private Set<Student> students = new HashSet<Student>();
+		
+}
---
+++ src/main/java/com/foxminded/entity/Student.java
@@ -0,0 +1,40 @@
+package com.foxminded.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@SuppressWarnings("serial")
+@Entity
+@Table (name = "students")
+@NoArgsConstructor
+@Data
+public class Student implements Serializable {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "second_name")
+	private String secondName;
+	
+	@ManyToOne (fetch=FetchType.LAZY, 
+			cascade = CascadeType.ALL)
+	@JoinColumn(name="group_id", insertable = false)
+	private Group group;
+
+}
