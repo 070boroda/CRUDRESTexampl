ba37dab586e06282fde81b97eb7f6dc63ca48929
1 <1@boroda>
2019-03-01 13:44:06 +0300

    commit

--- src/main/java/com/foxminded/entity/Group.java
+++ src/main/java/com/foxminded/entity/Group.java
@@ -37,42 +37,17 @@ public class Group implements Serializable {
 	@OneToMany(mappedBy="group", fetch=FetchType.EAGER
     , cascade = CascadeType.ALL)
 	private Set<Student> students = new HashSet<Student>();
+	
+	@OneToMany(mappedBy="group", fetch=FetchType.EAGER,
+			cascade = CascadeType.ALL)
+	private Set<Field> fields = new HashSet<Field>();
 
 	public Group(Integer id) {
 		super();
 		this.id = id;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((groupName == null) ? 0 : groupName.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Group other = (Group) obj;
-		if (groupName == null) {
-			if (other.groupName != null)
-				return false;
-		} else if (!groupName.equals(other.groupName))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+	
 
 	@Override
 	public String toString() {
