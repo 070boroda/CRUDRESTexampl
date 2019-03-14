46b4a4573387cb68c0cbb45cd46f2accf474f29c
ALIAKSANDR <zely1984@gmail.com>
2019-03-14 18:37:07 +0300

    add findOneGroup

--- src/main/java/com/foxminded/controller/GroupRESTController.java
+++ src/main/java/com/foxminded/controller/GroupRESTController.java
@@ -1,8 +1,11 @@
 package com.foxminded.controller;
 
+import com.foxminded.Exception.NotFoundException;
 import com.foxminded.entity.Group;
 import com.foxminded.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,14 +19,22 @@ public class GroupRESTController {
 
     @GetMapping
     @RequestMapping
-    public List<Group> findAll (){
-        return groupRepository.findAll();
+    public ResponseEntity<List<Group>> findAll(){
+        List<Group> groups = groupRepository.findAll();
+        if(groups.isEmpty()){
+            return new ResponseEntity<>(groups, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(groups,HttpStatus.OK);
     }
 
     @GetMapping
     @RequestMapping(value = "/{id}")
-    public Group findGroup(@PathVariable(name = "id")Integer id){
-        return groupRepository.findOne(id);
+    public ResponseEntity<?> findOneGroup(@PathVariable(name = "id")Integer id){
+        Group group = groupRepository.findOne(id);
+        if(group == null){
+            return new ResponseEntity<>(new NotFoundException("id" + id), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(group,HttpStatus.OK);
     }
 
 
