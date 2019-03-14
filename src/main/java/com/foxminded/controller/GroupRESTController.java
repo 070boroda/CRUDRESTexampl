043cda3d5ac14d0bb6d3e2be7a98240aec5f6776
ALIAKSANDR <zely1984@gmail.com>
2019-03-14 22:01:14 +0300

    REST all works

--- src/main/java/com/foxminded/controller/GroupRESTController.java
+++ src/main/java/com/foxminded/controller/GroupRESTController.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.Serializable;
 import java.util.List;
+
 
 @RestController
 public class GroupRESTController {
@@ -17,20 +19,47 @@ public class GroupRESTController {
     GroupRepository groupRepository;
 
     @GetMapping("/group")
-    public List<Group> findAll(){
-        return groupRepository.findAll();
+    ResponseEntity<List<Group>> findAllGroup(){
+        List<Group> groups = groupRepository.findAll();
+        if(groups.isEmpty()){
+            return new ResponseEntity<>(groups,HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(groups,HttpStatus.OK);
     }
 
     @GetMapping("/group/{id}")
-    public Group findOne(@PathVariable Integer id){
-        return groupRepository.findOne(id);
-         //.orElseThrow(()-> new NotFoundException(id));
+    public ResponseEntity<?> findOne(@PathVariable Integer id){
+        Group group = groupRepository.findOne(id);
+        if(group == null){
+            return new ResponseEntity<>(new NotFoundException(id), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(group,HttpStatus.OK);
     }
 
     @PostMapping("/group")
-    Group newGroup(@RequestBody Group group) {
-        return groupRepository.save(group);
+    ResponseEntity<? extends Serializable> newGroup(@RequestBody Group group) {
+        groupRepository.save(group);
+        return new ResponseEntity<>(group, HttpStatus.CREATED);
     }
 
+    @PutMapping("/group/{id}")
+    ResponseEntity<?> updateGroupe(@PathVariable Integer id,@RequestBody Group group){
+        Group groupFromDb = groupRepository.findOne(id);
+        if(groupFromDb == null){
+            return new ResponseEntity<>(new NotFoundException(id), HttpStatus.NOT_FOUND);
+        }
+        groupFromDb.setGroupName(group.getGroupName());
+        groupRepository.save(groupFromDb);
+        return new ResponseEntity<>(groupFromDb, HttpStatus.OK);
+    }
 
+    @DeleteMapping("/group/{id}")
+    ResponseEntity<?> deleteGroup(@PathVariable Integer id){
+        Group group = groupRepository.findOne(id);
+        if(group == null){
+            return new ResponseEntity<>(new NotFoundException(id), HttpStatus.NOT_FOUND);
+        }
+        groupRepository.delete(id);
+        return new ResponseEntity<Group>(HttpStatus.NO_CONTENT);
+    }
 }
